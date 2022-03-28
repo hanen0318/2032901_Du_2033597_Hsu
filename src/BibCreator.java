370ca1f4ad5b7a848998395a4d23c2db026862bf
frogboyxd <darrylkhoadu@hotmail.com>
2022-03-28 15:15:15 -0400

    initial commit
    initial commit

---
+++ src/BibCreator.java
@@ -0,0 +1,16 @@
+// -----------------------------------------------------
+// Final Project (BibCreator)
+// © Darryl Du, Hanen Hsu
+// Written by: Darryl Du 2032901, Hanen Hsu
+// -----------------------------------------------------
+import java.io.*;
+import java.util.Scanner;
+
+public class BibCreator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
