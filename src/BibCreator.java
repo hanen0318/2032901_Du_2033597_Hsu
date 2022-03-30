f8b96faaedea17cbc9ddd324bde469f79ce3852e
frogboyxd <darrylkhoadu@hotmail.com>
2022-03-30 01:42:53 -0400

    commit

--- src/BibCreator.java
+++ src/BibCreator.java
@@ -1,16 +1,42 @@
 // -----------------------------------------------------
 // Final Project (BibCreator)
-// © Darryl Du, Hanen Hsu
-// Written by: Darryl Du 2032901, Hanen Hsu
+// ©2022 Darryl Du, Hanen Hsu
+// Written by: Darryl Du 2032901, Hanen Hsu 2033597
 // -----------------------------------------------------
-import java.io.*;
-import java.util.Scanner;
+import java.util.Scanner; //scanner
+import java.io.FileInputStream; //reading data
+import java.io.FileOutputStream; //writing data
+import java.util.StringTokenizer; //string Tokenizer
+import java.io.BufferedReader; //read text file
+import java.util.regex.Matcher; //import for pattern matching
+import java.util.regex.Pattern; //import for pattern matching
+import java.io.File; //file and directory pathnames
+import java.io.FileNotFoundException; //exception for reading and writing
+import java.io.FileReader; //used to read data (in characters) from files
+import java.io.IOException;//exception
+import java.io.PrintWriter;//print the formatted representation of objects to the text-output stream
 
 public class BibCreator {
 
+	static int invalid = 0, valid = 0;//how many valid or invalid files
+	
+	public static String readFile(Scanner sc)
+	{
+		String s = "";
+		while (sc.hasNextLine()) //While the file still has a next line and is not at END OF FILE
+		{
+			s += sc.nextLine(); //add to the String
+		}
+		sc.close(); //Close Scanner
+		return s; //return the file
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("******Welcome to Bib Creator!******");
+		
+		
 	}
 
 }
--- src/FileInvalidException.java
+++ src/FileInvalidException.java
@@ -1,5 +1,22 @@
-
-
-public class FileInvalidException {
-
+// -----------------------------------------------------
+// Final Project (BibCreator)
+// ©2022 Darryl Du, Hanen Hsu
+// Written by: Darryl Du 2032901, Hanen Hsu 2033597
+// -----------------------------------------------------
+public class FileInvalidException extends Exception{
+	
+	public FileInvalidException()
+	{
+		super("Error: Input file cannot be parsed due to missing information (i.e. month={}, title={}, etc.)");
+	}
+	
+	public FileInvalidException(String message)
+	{
+		super(message);
+	}
+	
+	public String getMesage()
+	{
+		return super.getMessage();
+	}
 }
